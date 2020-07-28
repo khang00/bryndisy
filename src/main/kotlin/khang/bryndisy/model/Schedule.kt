828f9f7b9e79de0da27d1f954c4cacf548eab780
khang00 <dlqkhang2@gmail.com>
2020-07-28 13:44:37 +0700

    delete schedule

--- src/main/kotlin/khang/bryndisy/model/Schedule.kt
+++
@@ -1,8 +0,0 @@
-package khang.bryndisy.model
-
-import org.bson.types.ObjectId
-import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
-
-@Document
-data class Schedule(@Id val id: ObjectId, val name: String, val tasks: List<Task>)
--- src/main/kotlin/khang/bryndisy/service/adapter/TasksOptimizer.kt
+++ src/main/kotlin/khang/bryndisy/service/adapter/TasksOptimizer.kt
@@ -1,15 +1,12 @@
 package khang.bryndisy.service.adapter
 
-import khang.bryndisy.model.Schedule
 import khang.bryndisy.model.Task
 import khang.bryndisy.model.User
-import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 import java.time.Duration
 import java.util.*
 
 
 interface TasksOptimizer {
     fun optimizeTasks(user: User): Optional<User>
-    fun optimizeTasks(tasks: List<Task>, offHours: Duration  = Duration.ofHours(16)): Optional<List<Task>>
+    fun optimizeTasks(tasks: List<Task>, offHours: Duration = Duration.ofHours(16)): Optional<List<Task>>
 }
